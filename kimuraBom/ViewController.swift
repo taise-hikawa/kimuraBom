f7dd4ed2811bf21a1f35dce37322c617803cc79f
taisei.hikawa <cmmkarubi@gmail.com>
2020-06-25 18:57:57 +0900

    Initial Commit

---
+++ kimuraBom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kimuraBom
+//
+//  Created by Sakurako Shimbori on 2020/06/25.
+//  Copyright © 2020 Taisei Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
